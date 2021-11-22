6653a9e8a3968a01704550e84700cb4748571021
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-11-23 00:35:41 +0600

    array push,pop,shift,unshift

--- 3 (Array)/3.2.php
+++ 3 (Array)/3.2.php
@@ -2,20 +2,15 @@
 
 $students = [ "Asif", "Arif", 123,"Musaib"];
 
-
-
 $students[1] = "ishmam"; // change a array element , array is mutable
 
-
-
-
-array_unshift($students, "Asif");
+//array_unshift($students, "Bro");
+$students[] = "jamal"; // this also work as array push
 array_push($students, "Anik");
 
-array_shift($students);
-
-$std = array_pop($students);
-echo $std."\n";
+//array_shift($students);
+//array_pop($students);
+//echo "removed item is $std"."\n";
 
 //forward
 $n = count($students);
@@ -23,9 +18,9 @@ for ($i=0; $i<$n; $i++)
   echo $students[$i]."\n";
 
 
-/* array_shift(); -> remove 1 element from first
-array_pop(); -> remove 1 element from last
+// /* array_shift(); -> remove 1 element from first
+// array_pop(); -> remove 1 element from last
 
-array_unshift(); -> insert new element at first 
-array_push(); -> insert new element at last
-*/
+// array_unshift(); -> insert new element at first 
+// array_push(); -> insert new element at last
+// */

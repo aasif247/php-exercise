d36aba5fb3deb049c13e257f1a294d793753ea96
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-05 20:11:47 +0600

    work with http verbs (https://docs.google.com/document/d/1DFDEIvFTN2jkZUlWIGJ8LAj_QvHfRRwJOrwt6R8jOMk/edit#heading=h.lp1jli6wlhk3)

---
+++ 4(HTML Webpage)/4.1/form.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+        <title>Form Example</title>
+        <style>
+            body{
+                margin-top:30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <h1>Our First Form</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam minus enim magni fugiat quos natus culpa aliquid quod eaque eum.</p>
+
+                    <p>
+                        <?php if(isset($_REQUEST['fname']) && !empty($_REQUEST['fname'])){ ?>
+                            First name:<?php echo $_REQUEST['fname']; ?> <br/>
+                        <?php } ?> 
+                        
+                        <?php if(isset($_REQUEST['lname']) && !empty($_REQUEST['lname'])){?>
+                            Last name:<?php echo $_REQUEST['lname'];?><br/>
+                        <?php } ?>                 
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <form method="POST">
+                        <label for="fname">First Name</label>
+                        <input type="text" name="fname" id="fname">
+
+                        <label for="lname">Last Name</label>
+                        <input type="text" name="lname" id="lname">
+
+                        <button types="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

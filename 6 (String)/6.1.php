5515955945ae01f3dfd80b8b9ff8cb331e2e127d
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-07 21:03:42 +0600

    types of string in php ( https://docs.google.com/document/d/1QGzBmeanEfHDrwM6IJcxkKbiN7Af0zn_49wRmZPBuEM/edit# )

--- 6 (String)/6.1.php
+++ 6 (String)/6.1.php
@@ -7,12 +7,21 @@ $string = "my name is $name \n \t New data\n "; // if we use ' ' here then it do
 echo $string;
 
 // EOD -> here doc
-// 'EOD' -> Nowdoc  when we use nowdoc it doesn't recognise any variable
 
 $heredoc = <<<EOD
+asif
+gadget shed {$name} \n
+new arrival \n
+EOD;
+
+echo $heredoc;
+
+// 'EOD' -> Nowdoc  when we use nowdoc it doesn't recognise any variable
+
+$nowdoc = <<<'EOD'
 asif
 gadget shed {$name} \n
 new arrival
 EOD;
 
-echo $heredoc;
+echo $nowdoc;

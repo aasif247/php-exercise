3c685034dbb734a2f3c9c8b3704ab7b8f11a35ce
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-08 00:07:03 +0600

    access the character from a string( https://docs.google.com/document/d/14zJIY0D0rZ1KsWYRCeJEH1gJY0zHXoONEINma7gV4Eg/edit# )

--- 6 (String)/6.3.php
+++ 6 (String)/6.3.php
@@ -1,14 +1,22 @@
 <?php
 $string = "Hello World";
 
+$length = strlen($string);
+echo substr($string, 
+$length-3);
+echo PHP_EOL;
+echo substr($string,-3,2);
+echo "\n";
 echo $string[0];
-echo $string[1];
-echo $string[2];
-echo $string[3];
-echo $string[4];
-echo $string[5];
-echo $string[6];
-echo $string[7];
-echo $string[8];
-echo $string[9];
-echo $string[10];
+echo "\n";
+// echo $string[-11];
+// echo $string[1];
+// echo $string[2];
+// echo $string[3];
+// echo $string[4];
+// echo $string[5];
+// echo $string[6];
+// echo $string[7];
+// echo $string[8];
+// echo $string[9];
+// echo $string[10];

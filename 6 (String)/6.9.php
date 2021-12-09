0703a5fb75ba185fc1be1bc49b74b08e8fe66bdb
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-09 19:02:37 +0600

    wordwrap in string ( https://docs.google.com/document/d/1pUIA8JlO5ycZCpldSqOaDGGFbraI6M52g_P8fcNVtvk/edit#heading=h.a82t1zhrmlf0 )

---
+++ 6 (String)/6.9.php
@@ -0,0 +1,6 @@
+<?php
+$str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia magni, eveniet consectetur minima recusandaesddddddddddddddddddddddddddddddddddddddd inventore mollitia illum necessitatibus velit consequuntur";
+
+// echo wordwrap($str,30);
+echo wordwrap($str,30,"<br/>",true);
+

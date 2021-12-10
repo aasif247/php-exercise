ee388395df7b7d7a0aa5cd29466ce9ebd2252b08
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-10 22:59:57 +0600

    file path tips for windows and is_writable function ( https://docs.google.com/document/d/1KObbQV0HI7eAidX9jJPMco_3HLPPqkvsV0N22llKtwM/edit# )

---
+++ 7(file)/7.3.php
@@ -0,0 +1,10 @@
+<?php
+
+$filename = "E:\\Xampp\htdocs\php-exercise\\7(file)\\data\\file2.txt";
+
+// using is_writable function because it checks that  file is writable or not
+
+if(is_writable($filename)){
+    echo " write all code here";
+}
+  

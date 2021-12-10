0fca109033ae4bb16b2b8d401ccdb076f01ce060
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-10 12:35:23 +0600

    read data from file ( https://docs.google.com/document/d/1P_UCP7cYEoc3J91xnZzIoyEpG4GlCgOuQZDeaIeSeTU/edit#heading=h.gporqj53ljr )

---
+++ 7(file)/7.1/7.1.php
@@ -0,0 +1,28 @@
+<?php
+$filename = "E:/Xampp/htdocs/php-exercise/7(file)/data/file.txt";
+if(is_readable($filename)){
+
+$fp = fopen($filename,'r');
+while($line = fgets($fp)){
+    echo $line;
+}
+// rewind($fp);
+// fseek($fp,-5,SEEK_END);
+// while($line = fgets($fp,4)){
+//     echo $line."-";
+// }
+
+// $line = fgets($fp);
+// echo $line;
+
+// $line = fgets($fp);
+// echo $line;
+fclose($fp);
+
+$data = file($filename);
+echo $data[1];
+print_r($data);
+
+echo file_get_contents($filename);
+
+}

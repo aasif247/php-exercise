efb6fc8294137fdd55e899c0510a04b5029a3e93
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-01-01 23:54:06 +0600

    discuss about namespace (https://docs.google.com/document/d/1GUA7p9XsSadOcJdbjMiRPzo5F1LjCSoXFygT_Rje67Y/edit)

---
+++ 11 (OOP foundation)/11.27/main.php
@@ -0,0 +1,7 @@
+<?php
+namespace Astronomy;
+include "planet.php";
+include "earth.php";
+$planet = new Planets\Planet(); // unqualified
+$planet = new \Astronomy\Planets\Planet(); // qualified
+$planet->getName();
---
+++ 11 (OOP foundation)/11.27/planet.php
@@ -0,0 +1,10 @@
+<?php
+namespace Astronomy\Planets;
+class Planet{
+    function getName(){
+        echo "Earth\n";
+    }
+}
+
+// $planet = new Planet();
+// $planet->getName();
